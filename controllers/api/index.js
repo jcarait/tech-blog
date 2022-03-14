eba0f66ac5c25078d0ef2d054506cb7d341b192a
Jonathan F-Carait <jonathan.carait@outlook.com>
2022-03-14 17:11:29 +1100

    updated variables on controllers, updated profile form and del button handler. setup files for seed

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
+const userRoutes = require('./userRoutes.js');
 const postRoutes = require('./postRoutes');
 
 router.use('/users', userRoutes);
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,6 +1,45 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#project-name').value.trim();
-  const 
-}
+  const title = document.querySelector('#post-title').value.trim();
+  const content = document.querySelector('post-content').value.trim();
+
+  if (title && content) {
+    const response = await fetch('/api/posts', {
+      method: 'POST',
+      body: JSON.stringify({ title, post }),
+      headers: { 'Content Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to create post');
+    }
+  }
+};
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/projects/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to delete post');
+    }
+  }
+};
+
+document
+  .querySelector('.new-post-form')
+  .addEventListener('submit', newFormHandler);
+
+document
+  .querySelector('.post-list')
+  .addEventListener('submit', delButtonHandler);
