1c586b6a9b69a0d3b4a634370f3af96b63d842f0
Jonathan F-Carait <jonathan.carait@outlook.com>
2022-03-22 22:53:03 +1100

    navbar done with color scheme for app

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
       include: [
         {
           model: User,
-          attributes: ['title'],
+          attributes: ['name'],
         },
       ],
     });
@@ -20,7 +20,7 @@ router.get('/', async (req, res) => {
     // Pass posts data and session flag into template
     res.render('homepage', {
       posts,
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -42,7 +42,7 @@ router.get('/post/:id', async (req, res) => {
 
     res.render('post', {
       ...post,
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -62,7 +62,7 @@ router.get('/profile', withAuth, async (req, res) => {
 
     res.render('profile', {
       ...user,
-      logged_in: true
+      logged_in: true,
     });
   } catch (err) {
     res.status(500).json(err);
